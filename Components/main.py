777b1c779d507a610ec7efd48f17e6d86ba6c3cf
sirbobisherenow <chetan.likes.nature@gmail.com>
2025-09-24 08:06:59 -0700

    more comman d

--- Components/main.py
+++ Components/main.py
@@ -1,18 +1,23 @@
 from Assets.Scripts.info import *
-import sys, time
+import sys, time, os
 
 connectedServer = "home"
 
 print("haking game")
 print(ver)
 while True:
-    error = False
+    error = True
     userInput = input("\033[1m>\033[0m ")
-
+    #install hackingTools
     if userInput.startswith("hackingTools --install"):
+        error = False
         hackingToolsInstalled = True
         import Assets.Scripts.OutputLogs.hackingToolsOutput
     
+    if userInput.startswith("servers --tree"):
+        error = False
+        if connectedServer == "home":
+            print("hi")
 
     #Close game functionality
     if userInput.startswith("shutdown"):
@@ -20,3 +25,12 @@ while True:
         print("Disconnecting from " + gameName)
         time.sleep(1.5)
         sys.exit()
+    
+    #clear output functionality
+    if userInput.startswith("clear"):
+        error = False
+        os.system('cls')
+
+    #Leave a blank space above this comment for ease of adding commands
+    if error == True:
+        print("Terminal: Command unrecognized")
