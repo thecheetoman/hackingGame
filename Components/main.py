8873e53145ec7008734b82440957106a6cb777b3
sirbobisherenow <chetan.likes.nature@gmail.com>
2025-10-27 17:56:37 -0700

    connect to server!

--- Components/main.py
+++ Components/main.py
@@ -14,54 +14,57 @@ print(ver)
 while True:
     error = True
     userInput = input(connectedServer + "/> ")
-    #Commented out since these lines are for testing purposes
-    #if (connectedServer == "home"):
-        #print("guh")
-    
-    #Install hackingTools
-    if userInput.startswith("hackingTools"):
-        if hackingToolsInstalled == False:
-            #Installation functionality
-            if "--install" in userInput:
-                error = False
-                import Assets.Scripts.OutputLogs.hackingToolsOutput
-                hackingToolsInstalled = True
-                error = False
-            #What to do if hackingTools is not installed
-            else:
-                print("Package 'hackingTools' is not installed")
-                error = False
-        
-        ##Write commands that use hackingTools here
-        if hackingToolsInstalled == True:
-            if "--connect" in userInput:
-                serverToConnect = userInput.replace("hackingTools --connect ", "", 1)
-                if serverToConnect in homeAvailableServers:
-                    connectToServer(serverToConnect)
-                    connectedServer = serverToConnect
+    #What to do if home client
+    if (connectedServer == "home"):
+        #Install hackingTools
+        if userInput.startswith("hackingTools"):
+            if hackingToolsInstalled == False:
+                #Installation functionality
+                if "--install" in userInput:
                     error = False
+                    import Assets.Scripts.OutputLogs.hackingToolsOutput
+                    hackingToolsInstalled = True
+                    error = False
+                #What to do if hackingTools is not installed
                 else:
-                    print("Server \n" + serverToConnect + "\' not found")
+                    print("Package 'hackingTools' is not installed")
                     error = False
+            
+            ##Write commands that use hackingTools here
+            if hackingToolsInstalled == True:
+                if "--connect" in userInput:
+                    serverToConnect = userInput.replace("hackingTools --connect ", "", 1)
+                    if serverToConnect in homeAvailableServers:
+                        connectToServer(serverToConnect)
+                        connectedServer = serverToConnect
+                        error = False
+                    else:
+                        print("Server \n" + serverToConnect + "\' not found")
+                        error = False
 
-    #Look for "victim" servers =D
-    if userInput.startswith("nmap"):
-        error = False
-        if connectedServer == "home":
-            homeTree()
+        #Look for "victim" servers =D
+        if userInput.startswith("nmap"):
+            error = False
+            if connectedServer == "home":
+                homeTree()
 
-    #Close game functionality
-    if userInput.startswith("shutdown"):
-        error = False
-        print("Disconnecting from " + gameName)
-        time.sleep(1.5)
-        sys.exit()
-    
-    #Clear terminal functionality that works on linux and windows
-    if userInput.startswith("clear"):
-        error = False
-        clearConsole()
+        #Close game functionality
+        if userInput.startswith("shutdown"):
+            error = False
+            print("Disconnecting from " + gameName)
+            time.sleep(1.5)
+            sys.exit()
         
+        #Clear terminal functionality that works on linux and windows
+        if userInput.startswith("clear"):
+            error = False
+            clearConsole()
+    elif (connectedServer in homeAvailableServers):
+        if userInput.startswith("shutdown"):
+            error = False
+            print("Disconnecting from " + gameName)
+            time.sleep(0.5)
+            sys.exit()
 
     #Leave a blank space above this comment for ease of adding commands
     if error == True:
